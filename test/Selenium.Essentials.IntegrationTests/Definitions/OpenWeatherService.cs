5023a3ecb54e6027218c0adcdca8cb9bacd7b4d5
Peter <peterrexj@hotmail.com>
2019-07-21 22:23:22 +1000

    adding integration tests

---
+++ test/Selenium.Essentials.IntegrationTests/Definitions/OpenWeatherService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Selenium.Essentials.Api.Framework;
+using Selenium.Essentials.Utilities.Extensions;
+
+namespace Selenium.Essentials.IntegrationTests.Definitions
+{
+    public class OpenWeatherService
+    {
+        public enum ResponseFormat
+        {
+            Json,
+            Xml,
+            Html
+        }
+        private const string __OpenWeatherDomain = "http://api.openweathermap.org";
+        private const string __OpenWeatherApiKey = "246f355cb15be9063138812ffcaa523c";
+
+        public dynamic GetForcastWeatherByCityName(string cityName, ResponseFormat format)
+        {
+            var route = GetWebQuery(city: cityName,
+                            forcast: true,
+                            asXml: format == ResponseFormat.Xml,
+                            asHtml: format == ResponseFormat.Html);
+
+            return new TestApiHttp()
+                .SetEnvironment(__OpenWeatherDomain)
+                .PrepareRequest(route)
+                .Get()
+                .ResponseBody
+                .ContentJson;
+        }
+
+        private string _contactString(string query) => query.Contains("?") ? "&" : "?";
+        private string GetWebQuery(string city, bool forcast, bool asXml, bool asHtml)
+        {
+            string query = "/data/2.5";
+
+            if (forcast)
+            {
+                query = $"{query}/forecast";
+            }
+            if (city.HasValue())
+            {
+                query = $"{query}{_contactString(query)}q={city}";
+            }
+            if (asXml)
+            {
+                query = $"{query}{_contactString(query)}mode=xml";
+            }
+            if (asHtml)
+            {
+                query = $"{query}{_contactString(query)}mode=html";
+            }
+            query = $"{query}{_contactString(query)}APPID={__OpenWeatherApiKey}";
+
+            return query;
+        }
+    }
+}
---
+++ test/Selenium.Essentials.IntegrationTests/Tests/OpenWeatherApiTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Selenium.Essentials.Api;
+using Selenium.Essentials.IntegrationTests.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Selenium.Essentials.IntegrationTests.Tests
+{
+    class OpenWeatherApiTests : TestApiBase
+    {
+        OpenWeatherService _openWeatherService;
+        
+        [SetUp]
+        public void Setup()
+        {
+            Utility.InitializeFramework();
+            _openWeatherService = new OpenWeatherService();
+        }
+
+        [TestCase("Sydney")]
+        public void OpenWeatherByCityName(string city)
+        {
+            var forcast = _openWeatherService.GetForcastWeatherByCityName(city, OpenWeatherService.ResponseFormat.Json);
+            var responseCodeWeatherApp = (string)forcast.cod;
+            responseCodeWeatherApp.Should().Be("200", $"The weather app was not able to get forcast for city: {city}");
+        }
+    }
+}
