4351a16119fec282027139ef2bad8890e239a714
Peter <peterrexj@hotmail.com>
2019-07-21 13:09:22 +1000

    adding Excel payload data attribute

---
+++ src/Selenium.Essentials/Selenium.Essentials/Api/Attributes/PayloadDataExcelAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Selenium.Essentials.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Selenium.Essentials.Api.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class PayloadDataExcelAttribute : Attribute
+    {
+        public ExcelData ExcelData { get; private set; }
+        public PayloadDataExcelAttribute(string filename, string worksheet, string key, string column)
+        {
+            ExcelData = new ExcelData(filename, worksheet, key, column, "");
+        }
+    }
+}
