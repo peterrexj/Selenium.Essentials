4cd8b22a04fc6ceadd06282516c0fc93096ca2b2
Peter <peterrexj@hotmail.com>
2019-07-28 22:11:36 +1000

    Unordered list control to the collection of custom controls

---
+++ src/Selenium.Essentials/Web/Controls/CustomControls/UnorderedListControl.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Selenium.Essentials
+{
+    public class UnorderedListControl : BaseControl
+    {
+        public UnorderedListControl(IWebDriver driver, By by, BaseControl parentControl = null, string description = null)
+           : base(driver, by, parentControl, description)
+        {
+        }
+
+        public int TotalItems => Driver.FindElements(By.XPath($"{XpathSelector}/li")).GetAllVisibleElements().Count;
+
+        public IEnumerable<WebControl> List
+        {
+            get
+            {
+                for (int index = 1; index <= TotalItems; index++)
+                {
+                    yield return new WebControl(Driver, By.XPath($"{XpathSelector}/li[{index}]"));
+                }
+            }
+        }
+    }
+}
