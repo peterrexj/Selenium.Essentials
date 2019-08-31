91d2f1a3a8d81b5f88481940d39821615b7197dc
Peter <peterrexj@hotmail.com>
2019-08-31 14:59:55 +1000

    adding comments to api http and api request

--- src/Selenium.Essentials/Api/Core/TestApiHttp.cs
+++ src/Selenium.Essentials/Api/Core/TestApiHttp.cs
@@ -4,11 +4,26 @@ using System.Text;
 
 namespace Selenium.Essentials
 {
+    /// <summary>
+    /// Base http class for performing api tests
+    /// </summary>
     public class TestApiHttp
     {
+        /// <summary>
+        /// Environment or domain path (example: https://www.yourdomain.com)
+        /// </summary>
         public Uri EnvironmentUri { get; private set; }
+
+        /// <summary>
+        /// Default headers required for the Api. Headers can be added based on the request you need to make.
+        /// </summary>
         public HeaderCollection CommonHeaders { get; set; }
 
+        /// <summary>
+        /// Set the environment or domain
+        /// </summary>
+        /// <param name="environment">example: https://www.yourdomain.com</param>
+        /// <returns></returns>
         public TestApiHttp SetEnvironment(string environment)
         {
             EnvironmentUri = new Uri(environment);
@@ -20,6 +35,12 @@ namespace Selenium.Essentials
             CommonHeaders = new HeaderCollection();
         }
 
+        /// <summary>
+        /// Prepare your api request and you need to pass the route information
+        /// Example: new TestApiHttp().SetEnvironment("https://www.yourdomain.com").PrepareRequest("/path/to/my/endpoint")
+        /// </summary>
+        /// <param name="path">endpoint or the route of your api</param>
+        /// <returns></returns>
         public TestApiRequest PrepareRequest(string path = "")
         {
             var request = new TestApiRequest(EnvironmentUri, path);
@@ -32,11 +53,12 @@ namespace Selenium.Essentials
 
             return request;
         }
+
+        /// <summary>
+        /// This response will hold the cookie information. For example when you perform a login using credentials
+        /// and set response received to this property, 
+        /// every subsequent request will try to use the cookies which was received for the login, which make all other request authenticated
+        /// </summary>
         public TestApiResponse LoginResponse { get; set; }
-
-        public void Login()
-        {
-
-        }
     }
 }
