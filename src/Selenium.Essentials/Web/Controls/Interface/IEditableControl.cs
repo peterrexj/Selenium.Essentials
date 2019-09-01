0b5205daf6a7499f8e56590f8bd331acb03c9e6d
Peter <peterrexj@hotmail.com>
2019-09-01 21:10:48 +1000

    fixing a merge conflict issue

---
+++ src/Selenium.Essentials/Web/Controls/Interface/IEditableControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Selenium.Essentials
+{
+    public interface IEditableControl
+    {
+        void Set(string value);
+        string Get();
+    }
+}
