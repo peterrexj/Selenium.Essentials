b6c46b0d37d65af7aa4f28b888d0b92678bf5ebd
Peter <peterrexj@hotmail.com>
2019-09-01 12:58:06 +1000

    adding comments to utility

--- src/Selenium.Essentials/Core/Utility.cs
+++ src/Selenium.Essentials/Core/Utility.cs
@@ -10,43 +10,112 @@ using System.Text;
 
 namespace Selenium.Essentials
 {
+    /// <summary>
+    /// General utility class
+    /// </summary>
     public static class Utility
     {
+        /// <summary>
+        /// Utility to access runtime
+        /// </summary>
         public static class Runtime
         {
+            /// <summary>
+            /// Get executing assembly
+            /// </summary>
             public static Assembly ExecutingAssembly => Assembly.GetExecutingAssembly();
+
+            /// <summary>
+            /// Get executing folder
+            /// </summary>
             public static string ExecutingFolder => Path.GetDirectoryName(ExecutingAssembly.Location);
+
+            /// <summary>
+            /// Get logs storage folder (executing folder + Logs)
+            /// </summary>
             public static string LogOutputFolder { get; set; } = Path.Combine(ExecutingFolder, "Logs");
 
+            /// <summary>
+            /// Get the assembly based on the name provided
+            /// The assembly is fetched from the stacktrace information and not by reading the assembly folder
+            /// </summary>
+            /// <param name="name">name of the assembly to fetch</param>
+            /// <returns>the assembly </returns>
             public static Assembly GetAssembly(string name) => new StackTrace().GetFrames()
                 .Select(x => x?.GetMethod().ReflectedType?.Assembly).Distinct()
                 .FirstOrDefault(x => x != null && (x.ManifestModule.Name.EqualsIgnoreCase(name) || 
                                     Path.GetFileNameWithoutExtension(x.ManifestModule.Name).EqualsIgnoreCase(name)));
 
+            /// <summary>
+            /// Get the calling assembly of the current invoking method from the stack trace
+            /// </summary>
             public static Assembly CallingAssembly => new StackTrace().GetFrames()
                 .Select(x => x?.GetMethod().ReflectedType?.Assembly).Distinct()
                 .LastOrDefault(x => x != null && x.GetReferencedAssemblies().Any(y => y.FullName == ExecutingAssembly.FullName));
 
+            /// <summary>
+            /// Get the caller methodbase of the current inoking method from the stack trace
+            /// </summary>
             public static MethodBase CallerMethod => new StackTrace().GetFrames()
                 .Where(x => x?.GetMethod().ReflectedType?.Assembly.FullName == CallingAssembly.FullName)
                 .Select(x => x.GetMethod())
                 .Distinct()
                 .LastOrDefault();
 
+            /// <summary>
+            /// Get the caller method name of the current invoking method from the stack trace
+            /// </summary>
+            /// <returns></returns>
             public static string GetCallerMethodName() => CallerMethod.Name;
+
+            /// <summary>
+            /// Get the caller type of the current invoking method from the stack trace
+            /// </summary>
             public static Type CallerType => CallerMethod.DeclaringType;
+
+            /// <summary>
+            /// Get the assembly full path of the current invoking assembly from the stack trace
+            /// </summary>
             public static string CallingAssemblyFullPath => CallingAssembly?.Location;
+
+            /// <summary>
+            /// Get the assembly folder of the current invoking assembly from the stack trace
+            /// </summary>
             public static string CallingAssemblyFolder => Path.GetDirectoryName(CallingAssemblyFullPath);
+
+            /// <summary>
+            /// Get the assembly name with no extention of the current invoking assembly from the stack trace
+            /// </summary>
             public static string CallingAssemblyNameWithNoExtension => Path.GetFileNameWithoutExtension(CallingAssemblyFullPath);
+
+            /// <summary>
+            /// Determine if the current execution is in debug mode based on debugger attached property
+            /// </summary>
             public static bool IsInDebugMode => System.Diagnostics.Debugger.IsAttached;
         }
 
+
+        /// <summary>
+        /// Application configuration information
+        /// </summary>
         public static class AppConfig
         {
+
+            /// <summary>
+            /// Load the assembly configuration based on the assembly name
+            /// </summary>
+            /// <param name="name">name of the assembly to load the configuration</param>
+            /// <returns>configuration information of the assembly</returns>
             public static Configuration LoadConfiguration(string name)
             {
                 return LoadConfiguration(Utility.Runtime.GetAssembly(name));
             }
+
+            /// <summary>
+            /// Load the assembly configuration based on the assembly
+            /// </summary>
+            /// <param name="assembly">Assembly to load the configuration</param>
+            /// <returns></returns>
             public static Configuration LoadConfiguration(Assembly assembly)
             {
                 if (assembly != null)
@@ -56,6 +125,11 @@ namespace Selenium.Essentials
                 return null;
             }
 
+            /// <summary>
+            /// Read the AppSettings from the assembly configuration
+            /// </summary>
+            /// <param name="assemblyName">Assembly name to load the configuration</param>
+            /// <returns>Dictinary with key value pair of the AppSettings</returns>
             public static Dictionary<string, string> ReadAppSettings(string assemblyName) => ReadAppSettings(Runtime.GetAssembly(assemblyName));
             public static Dictionary<string, string> ReadAppSettings(Assembly assembly)
             {
@@ -71,6 +145,10 @@ namespace Selenium.Essentials
             }
 
             private static Configuration _configurationCaller;
+
+            /// <summary>
+            /// Configuration of the caller assembly
+            /// </summary>
             public static Configuration ConfigurationCaller
             {
                 get
@@ -84,6 +162,10 @@ namespace Selenium.Essentials
             }
 
             private static Dictionary<string, string> _appsettingsCallerAssembly;
+
+            /// <summary>
+            /// AppSettings of the caller assembly
+            /// </summary>
             public static Dictionary<string, string> AppSettingsCallerAssembly
             {
                 get
@@ -97,13 +179,25 @@ namespace Selenium.Essentials
             }
         }
 
+        /// <summary>
+        /// Tablet window size.
+        /// Default to Size(768, 1024)
+        /// </summary>
         public static Size TabletWindowSize { get; set; } = new Size(768, 1024);
+
+        /// <summary>
+        /// Mobile window size
+        /// Default to Size(375, 667)
+        /// </summary>
         public static Size MobileWindowSize { get; set; } = new Size(375, 667);
 
+        /// <summary>
+        /// Initialize the selenium essentials framework.
+        /// Create a test context and initialze the logging
+        /// </summary>
         public static void InitializeFramework()
         {
             TestContextHelper.CreateTestContext();
         }
-
     }
 }
