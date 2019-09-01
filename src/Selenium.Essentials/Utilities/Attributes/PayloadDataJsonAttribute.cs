826568cb71cea2509c7009c78d952a0c0234243b
Peter <peterrexj@hotmail.com>
2019-09-01 14:08:29 +1000

    comments to json payload attribute

--- src/Selenium.Essentials/Utilities/Attributes/PayloadDataJsonAttribute.cs
+++ src/Selenium.Essentials/Utilities/Attributes/PayloadDataJsonAttribute.cs
@@ -6,12 +6,26 @@ using System.Text;
 
 namespace Selenium.Essentials
 {
+    /// <summary>
+    /// Json Payload attribute for nUnit Api and Web tests
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
     public class PayloadDataJsonAttribute : Attribute
     {
+        /// <summary>
+        /// Relative path to the json file
+        /// </summary>
         public string FilePath { get; set; }
+
+        /// <summary>
+        /// Content of the json file, which will be loaded during initialization
+        /// </summary>
         public string FileContent { get; private set; }
 
+        /// <summary>
+        /// Read the json file and exposed through FileContent property
+        /// </summary>
+        /// <param name="filePath">path to the json file (relative path)</param>
         public PayloadDataJsonAttribute(string filePath)
         {
             if (!File.Exists(filePath))
