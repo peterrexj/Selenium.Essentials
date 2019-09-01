de934b86a51d2655b4c7a6e05dd71db6bb698694
Peter <peterrexj@hotmail.com>
2019-09-01 14:22:22 +1000

    comment on the button control

--- src/Selenium.Essentials/Web/Controls/CustomControls/ButtonControl.cs
+++ src/Selenium.Essentials/Web/Controls/CustomControls/ButtonControl.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace Selenium.Essentials
 {
+    /// <summary>
+    /// Button control to use in page object. 
+    /// </summary>
     public class ButtonControl : BaseControl
     {
         public ButtonControl(IWebDriver driver, By by, BaseControl parentControl = null, string description = null, bool firstAvailable = false)
