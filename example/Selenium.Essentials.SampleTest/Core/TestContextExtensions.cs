7d426021c5a154bc16931045e4e7f1fed64410a1
peterrexj <peterrexj@hotmail.com>
2022-07-15 13:09:16 +1000

    Test Context extension to extract the test name

---
+++ example/Selenium.Essentials.SampleTest/Core/TestContextExtensions.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Pj.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Essentials.SampleTest.Core
+{
+    public static class TestContextExtensions
+    {
+        /// <summary>
+        /// Gives the Test Name from the nUnit test context by removing special chars and adding whitespace
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static string TestName(this TestContext context)
+            => context.Test.Name.RemoveSpecialChars().ApplyWhitespaceOverCaps().ReplaceMultiple(" ", "_", "  ");
+    }
+}
