e76c8e127a1bc2101fd86f841d609cf1123eb7e1
Joseph, Peter <Peter.Joseph@cba.com.au>
2023-09-29 12:59:56 +1000

    multiform data upload

---
+++ src/TestAny.Essentials.Core/Dtos/Api/MultipartFormDataProperty.cs
@@ -0,0 +1,25 @@
+﻿namespace TestAny.Essentials.Core.Dtos.Api
+{
+    public class MultipartFormDataProperty
+    {
+        public enum ContentTransmitTypeEnum
+        {
+            AsString,
+            AsFile
+        }
+
+        public enum ContentStreamTypeEnum
+        {
+            Stream,
+            Text
+        }
+
+        public string PropertyName { get; set; }
+        public string PropertyValue { get; set; }
+
+        public ContentTransmitTypeEnum ContentTransmitType { get; set; }
+        public ContentStreamTypeEnum ContentStreamType { get; set; }
+
+        public string ContentType { get; set; }
+    }
+}
