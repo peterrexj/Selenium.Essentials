c400fde6cd49520f26bd142cd6c3bfa3742ad93d
peterrexj <peterrexj@hotmail.com>
2024-05-01 20:42:11 +1000

    next release draft

--- buildpack/PublishProjects.ps1
+++ buildpack/PublishProjects.ps1
@@ -1,7 +1,7 @@
 ﻿Set-ExecutionPolicy Bypass
 
 $apiKey = [System.Environment]::GetEnvironmentVariable('NugetApiKey', 'User')
-$packageVersion = '.1.0.6.nupkg'
+$packageVersion = '.1.0.6.1.nupkg'
 
 $score = [IO.Path]::Combine($PSScriptRoot, '..\Output\TestAny.Essentials.Core' + $packageVersion)
 $sapi = [IO.Path]::Combine($PSScriptRoot, '..\Output\TestAny.Essentials.Api' + $packageVersion)
@@ -13,7 +13,7 @@ Get-ChildItem -Path $sapi -ErrorAction Stop
 Get-ChildItem -Path $sapiLite -ErrorAction Stop
 Get-ChildItem -Path $se -ErrorAction Stop
 
-# dotnet nuget push $score --api-key $apiKey --source https://api.nuget.org/v3/index.json
-# dotnet nuget push $sapi --api-key $apiKey --source https://api.nuget.org/v3/index.json
-# dotnet nuget push $sapiLite --api-key $apiKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push $score --api-key $apiKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push $sapi --api-key $apiKey --source https://api.nuget.org/v3/index.json
+dotnet nuget push $sapiLite --api-key $apiKey --source https://api.nuget.org/v3/index.json
 dotnet nuget push $se --api-key $apiKey --source https://api.nuget.org/v3/index.js
