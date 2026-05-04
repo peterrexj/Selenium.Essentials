65af08cdc04c0e91372f7b5372d22a4426644e3c
peterrexj <peterrexj@hotmail.com>
2026-05-04 13:36:56 +1000

    2.0.6

--- buildpack/PublishProjects.ps1
+++ buildpack/PublishProjects.ps1
@@ -1,7 +1,7 @@
 ﻿Set-ExecutionPolicy Bypass
 
 $apiKey = [System.Environment]::GetEnvironmentVariable('NugetApiKey', 'User')
-$packageVersion = '.2.0.6.nupkg'
+$packageVersion = '.2.0.7.nupkg'
 
 $score = [IO.Path]::Combine($PSScriptRoot, '..\Output\TestAny.Essentials.Core' + $packageVersion)
 $sapi = [IO.Path]::Combine($PSScriptRoot, '..\Output\TestAny.Essentials.Api' + $packageVersion)
